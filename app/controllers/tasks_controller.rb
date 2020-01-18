1028c249bbec702a9fc26ba13e630026d17207c0
hei.yoshii <y.heihachiro@gmail.com>
2020-01-18 05:04:35 +0000

    add column

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -48,8 +48,11 @@ class TasksController < ApplicationController
     flash[:success] = 'Task は正常に削除されました'
     redirect_to tasks_url
   end
+
+  private
+
+  def task_params
+    params.require(:task).permit(:content, :status)
+  end
 end
 
-def task_params
-  params.require(:task).permit(:content)
-end
