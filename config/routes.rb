bcc45a32c70aeaa2233f592a0daf29e96b64380d
hei.yoshii <y.heihachiro@gmail.com>
2020-01-25 00:29:19 +0000

    user login

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,18 @@
 Rails.application.routes.draw do
-  get 'toppages/index'
+  get 'sessions/new'
+  get 'sessions/create'
+  get 'sessions/destroy'
+  root to: 'tasks#index'
+
+  get 'login', to: 'sessions#new'
+  post 'login', to: 'sessions#create'
+  delete 'logout', to: 'sessions#destroy'
+
+#  get 'toppages/index'
   get 'users/index'
   get 'users/show'
   get 'users/new'
   get 'users/create'
-  root to: 'tasks#index'
   
   get 'signup', to: 'users#new'
   resources :users, only: [:index, :show, :new, :create]
