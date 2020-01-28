05b4d3edd95f7c6c6051597e8626f339cca9a48d
hei.yoshii <y.heihachiro@gmail.com>
2020-01-28 05:31:04 +0000

    set correct_user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -20,7 +20,8 @@ class UsersController < ApplicationController
     
     if @user.save
       flash[:success] = 'ユーザを登録しました。'
-      redirect_to @user
+#      redirect_to tasks_url
+      redirect_to :root
     else
       flash.now[:danger] = 'ユーザの登録に失敗しました。'
       render :new
