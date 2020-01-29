d195b6f4107ad71b4cd949c1af15f7ba224454ed
hei.yoshii <y.heihachiro@gmail.com>
2020-01-29 02:27:42 +0000

    fix html

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,16 +1,6 @@
 class UsersController < ApplicationController
   before_action :require_user_logged_in, only: [:index, :show]
 
-#  def index
-#    @users = User.order(id: :desc).page(params[:page]).per(25)
-#  end
-
-#  def show
-#    @user = User.find(params[:id])
-#    @tasks = @user.tasks.order(id: :desc).page(params[:page])
-#    @task = Task.new
-#  end
-
   def new
     @user = User.new
   end
@@ -20,7 +10,6 @@ class UsersController < ApplicationController
     
     if @user.save
       flash[:success] = 'ユーザを登録しました。'
-#      redirect_to tasks_url
       redirect_to :root
     else
       flash.now[:danger] = 'ユーザの登録に失敗しました。'
